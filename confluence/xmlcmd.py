2da2ee139350efa756b6022705e7c7a59bdd9b6a
Yasu Watanabe <ywata@protonmail.com>
2023-03-05 18:28:13 +0900

    Feature: Implement possible generic mechanisim to modify XML.

---
+++ confluence/xmlcmd.py
@@ -0,0 +1,118 @@
+import copy
+from dataclasses import dataclass
+from xml.etree import ElementTree as ET
+from xml.etree import ElementPath as EP
+from collections import deque
+@dataclass
+class GetXPath:
+    xpath:str
+@dataclass
+class CopyNode:
+    pass
+@dataclass
+class DupNode:
+    pass
+@dataclass
+class PopNode:
+    pass
+
+@dataclass
+class Push:
+    elem: ET.Element
+
+@dataclass
+class CallFunction:
+    fun : object
+    args : object
+    conv : object
+
+@dataclass
+# Inject data on data_stack into the second node.
+class Insert:
+    nth:int
+
+@dataclass
+class Remove:
+    pass
+Cmd = GetXPath | CopyNode | DupNode | Insert | Remove | Push | CallFunction
+
+@dataclass
+class Node:
+    elem: ET.Element
+@dataclass
+class Null:
+    pass
+Data = Node | Null
+
+def interp(cmd_stack:deque, data_stack)-> Data:
+    if len(cmd_stack) == 0:
+        return data_stack
+    curr_cmd = cmd_stack.popleft()
+
+    match curr_cmd:
+        case GetXPath(xp):
+            assert len(data_stack) > 0
+            node = data_stack[0]
+            match node:
+                case Node(elm):
+                    r = EP.find(elm, xp)
+                    if r is None:
+                        data_stack.appendleft(Null())
+                    else:
+                        data_stack.appendleft(Node(r))
+                case _ :
+                    raise Exception("Invalid node")
+
+        case CopyNode():
+            assert len(data_stack) > 0
+            org_node = data_stack.popleft()
+            new_node = copy.deepcopy(org_node)
+            data_stack.appendleft(new_node)
+        case DupNode():
+            assert len(data_stack) > 0
+            org_node = data_stack[0]
+            data_stack.appendleft(org_node)
+        case PopNode():
+            assert len(data_stack) > 0
+            data_stack.popleft()
+        case Remove():
+            assert len(data_stack) >= 2
+            node = data_stack.popleft()
+            top = data_stack[0]
+            match node, top:
+                case Node(elm), Node(root):
+                    root.remove(elm)
+                case _,_:
+                    raise Exception("Invalid Node")
+        case Push(elm):
+            data_stack.appendleft(Node(elm))
+        case Insert(nth):
+            assert nth >= 0
+            assert len(data_stack) >= 2
+            node = data_stack.popleft()
+            top = data_stack[0]
+            match node, top:
+                case Node(elm), Node(root):
+                    root.insert(nth, elm)
+                case _,_:
+                    raise Exception("Invalid Node")
+        case CallFunction(func, args, conv):
+            assert func is not None
+            res = func(*args)
+            if res is not None:
+                data_stack.appendleft(Node(conv(res)))
+            else:
+                data_stack.appendleft(Null)
+
+    res = interp(cmd_stack, data_stack)
+    return res
+
+
+def interpreter(ls:list, root:ET.Element):
+    cq = deque(ls)
+    dq = deque([Node(root)])
+    return interp(cq, dq)
+
+
+
+
--- tests/test_xml_processing.py
+++ tests/test_xml_processing.py
@@ -1,5 +1,6 @@
 import unittest
 from confluence.content import analize_parse_error
+from confluence.xmlcmd import *
 def test_analize_parse_error_empty():
     patterns = []
     res = analize_parse_error([], "msg", "parsed data")
@@ -26,4 +27,132 @@ def test_analize_parse_error_use_data():
         return f"{matched.group(1)} {matched.group(2)}"
     res = analize_parse_error([(rex, lambda _, data: data)], msg, "use data")
     assert res == "use data"
+
+def test_interp_empty_stack():
+    xml = """
+    <root>
+    <h1>text</h1>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    res = interpreter([], root)
+    assert [*res] == [Node(root)]
+
+def test_interp_get_valid():
+    xml = """
+    <root>
+    <h1>text</h1>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    res = interpreter([GetXPath("./h1")], root)
+    assert res[0].elem.tag == "h1"
+
+def test_interp_get_invalid():
+    xml = """
+    <root>
+    <h1>text</h1>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    res = interpreter([GetXPath("./hx")], root)
+    assert [*res] == [Null(), Node(root)]
+
+def test_interp_copy():
+    xml = """
+    <root>
+    <h1>text</h1>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    res = interpreter([CopyNode()], root)
+    assert res[0].elem.tag == "root"
+    assert res[0].elem != root
+
+def test_interp_dup():
+    xml = """
+    <root>
+    <h1>text</h1>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    res = interpreter([DupNode()], root)
+
+    assert len(res) == 2
+    assert res[1].elem == root
+    assert res[0].elem == root
+
+
+def test_interp_pop():
+    xml = """
+    <root>
+    <h1>text</h1>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    res = interpreter([PopNode()], root)
+    assert len(res) == 0
+
+def test_interp_remove():
+    xml = """
+    <root>
+    <h1>text1</h1>
+    <h1>text2</h1>    
+    <hex></hex>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    res = interpreter([GetXPath("h1"), Remove()], root)
+    h1 = EP.find(root, "h1")
+    assert h1.text == "text2"
+
+def test_interp_push():
+    xml = """
+    <root>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    res = interpreter([Push(ET.Element("tag"))], root)
+    assert len(res) == 2
+    assert res[0].elem.tag == "tag"
+def test_interp_insert():
+    xml = """
+    <root>
+    <elem1></elem1>
+    <elem2></elem2>
+    <elem3></elem3>
+    <elem4></elem4>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    res = interpreter([Push(ET.Element("tag")), Insert(1)], root)
+    assert res[0].elem[1].tag == "tag"
+
+def test_interp_call_function_valid_tuple():
+    xml = """
+    <root>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    def fun(tag1, tag2):
+        return tag1 + tag2
+    def conv(tag):
+        return ET.Element(tag)
+
+    res = interpreter([CallFunction(fun, ("tag1", "tag2"), conv)], root)
+    assert res[0].elem.tag == "tag1tag2"
+
+def test_interp_call_function_valid_single():
+    xml = """
+    <root>
+    </root>
+    """
+    root = ET.fromstring(xml)
+    def fun(tag1):
+        return tag1
+    def conv(tag):
+        return ET.Element(tag)
+    # This is tricky pattern to pass generic argument for general function to work.
+    res = interpreter([CallFunction(fun, ("tag1",), conv), Insert(1)], root)
+    assert res[0].elem[0].tag == "tag1"
 
