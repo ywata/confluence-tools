3e995bd4655bbdd37d1000da837d4d26e13cc945
Yasu Watanabe <ywata@protonmail.com>
2023-04-10 19:52:19 +0900

    Feature: Prepare to re-implement API code based on new parser.

--- jsonschema/api.py
+++ jsonschema/api.py
@@ -2,3 +2,24 @@ from jsonschema.schema import *
 from dataclasses import make_dataclass, field, dataclass
 
 import pprint as pp
+
+def traverse_schema(schema:JsonSchema, fun):
+    if schema.schema:
+        fun(schema.schema)
+    if schema.ref:
+        fun(schema.ref)
+    if schema.defn:
+        for (key, val) in schema.defn.items():
+            fun(val)
+
+def generate_api(schema:JsonSchema):
+    top = schema.ref
+    top_schema = schema.defn[top]
+    aliases = top_schema.attributes['content'][0].constraints['items'].constraints
+    for alias in aliases:
+        alias_schema = schema.defn[alias.ref]
+        normalized_schema = replace_ref(alias_schema, schema)
+        print(alias)
+        pp.pprint(normalized_schema)
+
+    return
--- tests/test_adf.py
+++ tests/test_adf.py
@@ -2,11 +2,17 @@ import pytest
 import unittest
 import jsonschema.schema
 import json
+import os
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 def test_create_adf_processor():
-    with open("../spec/adf-schema.json", "r") as f:
+    with open(os.path.join(dir_path, "../spec/adf-schema.json"), "r") as f:
         schema_dict = json.load(f)
-        res = jsonschema.schema.parse_json_schema(schema_dict)
+        jsc = jsonschema.schema.parse_json_schema(schema_dict)
+
+        jsc= jsonschema.schema.normalize_schema((jsc))
+        nodes = list(jsc.defn.keys())
+
         pass
 
 
